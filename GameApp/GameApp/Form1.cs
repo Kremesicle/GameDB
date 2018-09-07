118ec28e0325e6f89588ad719114807d41859ead
CJ Osmond <runescapegothacked619@hotmail.com>
2018-09-07 18:42:37 -0230

    Initial commit

---
+++ GameApp/GameApp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+namespace GameApp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+          
+            using (var cn = GetConnection.OpenConnection())
+            {
+                var data = cn.Query("SELECT * FROM Testing");
+                foreach(var item in data)
+                {
+                    lstGame.Items.Add(item);
+                }
+            }
+        }
+    }
+}
---
+++ GameApp/GameApp/GetConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace GameApp
+{
+    public class GetConnection
+    {       
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection sn = new SqlConnection();
+            sn.ConnectionString = @"Data Source=DESKTOP-CRMGFLF\SQLEXPRESS;Database=TestTable;Integrated Security=True;";
+            sn.Open();
+            return sn;
+        }
+    }
+}
